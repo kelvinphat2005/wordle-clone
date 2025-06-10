8f1822af6c359642f71dbd6c8c98381201c33ee2
Kelvin Phat <kelvinphat12@gmail.com>
2025-06-10 16:50:29 -0700

    try 1213123

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wordle-clone/',
+  base: 'https://kelvinphat2005.github.io/wordle-clone/',
   plugins: [react(), tailwindcss()],
 })
